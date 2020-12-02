7de223b758faa0f663ce03b54b698cf5cecc9735
joinsiyuchun <348977654@qq.com>
2020-12-02 22:38:56 +0800

    modified:   mayan/apps/cabinets/locale/zh_Hans/LC_MESSAGES/django.mo 	modified:   mayan/apps/cabinets/locale/zh_Hans/LC_MESSAGES/django.po 	modified:   mayan/apps/cabinets/menus.py 	modified:   mayan/apps/checkouts/locale/zh_Hans/LC_MESSAGES/django.mo 	modified:   mayan/apps/checkouts/locale/zh_Hans/LC_MESSAGES/django.po 	modified:   mayan/apps/document_states/locale/zh_Hans/LC_MESSAGES/django.mo 	modified:   mayan/apps/documents/locale/zh_Hans/LC_MESSAGES/django.mo 	modified:   mayan/apps/documents/locale/zh_Hans/LC_MESSAGES/django.po 	modified:   mayan/apps/documents/menus.py

--- mayan/apps/documents/menus.py
+++ mayan/apps/documents/menus.py
@@ -5,6 +5,6 @@ from mayan.apps.navigation.classes import Menu
 from .icons import icon_menu_documents
 
 menu_documents = Menu(
-    icon_class=icon_menu_documents, label=_('Documents'),
+    icon_class=icon_menu_documents, label=_('Documents Management'),
     name='documents'
 )
