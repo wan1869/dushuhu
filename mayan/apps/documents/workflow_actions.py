698007ea35de809157685fb0cc2b29ab5ad98c02
joinsiyuchun <348977654@qq.com>
2021-01-10 23:06:32 +0800

    modified:   mayan/apps/documents/workflow_actions.py

--- mayan/apps/documents/workflow_actions.py
+++ mayan/apps/documents/workflow_actions.py
@@ -13,13 +13,12 @@ class TrashDocumentAction(WorkflowAction):
         context['document'].delete()
 
 #客户化 新版本上传 state action
-class NewDocumentVersionAction(WorkflowAction):
-    label = _('Upload as new version')
+class CopyMetadataAction(WorkflowAction):
+    label = _('Copy metadata to maste file')
 
     def execute(self, context):
         document_uuid = context['document'].description
         document_id = context['document'].pk
-        # document = Document.objects.get(id=document_id)
         document = get_object_or_404(klass=Document, uuid=document_uuid)
         #copy metadata
         metadata = DocumentMetadata.objects.filter(metadata_type__name="creator", document_id=document_id)
@@ -38,6 +37,24 @@ class NewDocumentVersionAction(WorkflowAction):
         effective_date = metadata3[0].value
         result3 = DocumentMetadata.objects.filter(metadata_type__name="effective_date", document_id=document.id)
         result3.update(value=effective_date)
+        metadata4 = DocumentMetadata.objects.filter(metadata_type__name="publish_date", document_id=document_id)
+        publish_date = metadata4[0].value
+        result4 = DocumentMetadata.objects.filter(metadata_type__name="publish_date", document_id=document.id)
+        result4.update(value=publish_date)
+        metadata5 = DocumentMetadata.objects.filter(metadata_type__name="expired_date", document_id=document_id)
+        expired_date = metadata5[0].value
+        result5 = DocumentMetadata.objects.filter(metadata_type__name="expired_date", document_id=document.id)
+        result5.update(value=expired_date)
+
+
+
+#客户化 新版本上传 state action
+class NewDocumentVersionAction(WorkflowAction):
+    label = _('Upload as new version')
+
+    def execute(self, context):
+        document_uuid = context['document'].description
+        document = get_object_or_404(klass=Document, uuid=document_uuid)
         #new version
         file_object = context['document'].latest_version.file
         document.new_version(
